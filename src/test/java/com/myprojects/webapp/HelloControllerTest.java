a9d41a0802e1de94aab780eab9c7880b55c7faf0
davide <davidebotti92@gmail.com>
2021-02-07 11:08:23 +0100

    Adds unit testing on the resource

---
+++ src/test/java/com/myprojects/webapp/HelloControllerTest.java
@@ -0,0 +1,36 @@
+package com.myprojects.webapp;
+
+import com.myprojects.domain.MyUseCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private MyUseCase myUseCase;
+
+
+    @Test
+    public void helloIsCalled() throws Exception {
+        mvc.perform(get("/hello")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        verify(myUseCase).operation();
+    }
+}
