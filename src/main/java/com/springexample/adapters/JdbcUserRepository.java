96819c55e3ec194f1092103d2b4d48cfa7047fe1
davide <davidebotti92@gmail.com>
2021-02-12 11:25:08 +0100

    Adds try catch in jdbc repository

--- src/main/java/com/springexample/adapters/JdbcUserRepository.java
+++ src/main/java/com/springexample/adapters/JdbcUserRepository.java
@@ -2,13 +2,14 @@ package com.springexample.adapters;
 
 import com.springexample.domain.User;
 import com.springexample.domain.UserRepository;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.Optional;
 
 public class JdbcUserRepository implements UserRepository {
 
-    private JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     public JdbcUserRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -16,23 +17,33 @@ public class JdbcUserRepository implements UserRepository {
 
     @Override
     public Optional<User> fetch(long userId) {
-        User user = jdbcTemplate.queryForObject("SELECT * FROM USERS WHERE ID=" + userId,
-            (resultSet, i) -> new User(
-                resultSet.getString("USERNAME"),
-                resultSet.getString("PASSWORD")
-            )
-        );
-        if (user != null) {
-            return Optional.of(user);
-        } else {
+        try {
+            User user = jdbcTemplate.queryForObject("SELECT * FROM USERS WHERE ID=" + userId,
+                (resultSet, i) -> new User(
+                    resultSet.getString("USERNAME"),
+                    resultSet.getString("PASSWORD")
+                )
+            );
+            if (user != null) {
+                return Optional.of(user);
+            } else {
+                return Optional.empty();
+            }
+        } catch (DataAccessException e) {
+            e.printStackTrace();
             return Optional.empty();
         }
     }
 
     @Override
     public Optional<Integer> addUser(User user) {
-        jdbcTemplate.update("INSERT INTO USERS (USERNAME, PASSWORD) VALUES (?, ?)", user.getName(), user.getPassword());
-        return Optional.of(1);
+        try {
+            jdbcTemplate.update("INSERT INTO USERS (USERNAME, PASSWORD) VALUES (?, ?)", user.getName(), user.getPassword());
+            return Optional.of(1);
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
     }
 
 }
