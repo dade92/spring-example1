0b3114432c4279e156def91c47f2ae8c5fb765ae
davide <davidebotti92@gmail.com>
2021-02-16 19:22:41 +0100

    Adds test controller for kotlin

---
+++ src/main/kotlin/com/springexample/webapp/PathController.kt
@@ -0,0 +1,15 @@
+package com.springexample.webapp
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PathController {
+
+    @GetMapping("path")
+    fun path(): ResponseEntity<Object> {
+        return ResponseEntity.noContent().build()
+    }
+
+}
---
+++ src/test/kotlin/com/springexample/webapp/PathControllerTest.kt
@@ -0,0 +1,26 @@
+package com.springexample.webapp
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@RunWith(SpringRunner::class)
+@WebMvcTest(PathController::class)
+class PathControllerTest {
+
+    @Autowired
+    private lateinit var mvc: MockMvc
+
+    @Test
+    fun pathIsCalled() {
+        mvc.perform(
+            get("/path").contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isNoContent)
+    }
+}
