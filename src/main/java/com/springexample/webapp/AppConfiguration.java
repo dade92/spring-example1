fb986ef41005293f7ab0701ea589073451639e6d
davide <davidebotti92@gmail.com>
2021-02-18 12:36:58 +0100

    Configures the datasource

--- src/main/java/com/springexample/webapp/AppConfiguration.java
+++ src/main/java/com/springexample/webapp/AppConfiguration.java
@@ -1,10 +1,20 @@
 package com.springexample.webapp;
 
+import com.springexample.adapters.JdbcUserRepository;
 import com.springexample.domain.MyUseCase;
 import com.springexample.domain.MyUseCaseImpl;
+import com.springexample.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableConfigurationProperties(
@@ -15,6 +25,27 @@ public class AppConfiguration {
     @Bean
     public MyUseCase myUseCase(TestConfiguration testConfiguration) {
         return new MyUseCaseImpl(testConfiguration);
+    }
+
+    @Bean
+    @Primary
+    public DataSource appDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url("jdbc:mysql://localhost:3306/spring-example");
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password("1191992Da");
+        return dataSourceBuilder.build();
+    }
+
+    @Bean
+    public UserRepository userRepository(
+        DataSource appDataSource
+    ) {
+        JdbcOperations jdbcOperations = new JdbcTemplate(appDataSource);
+
+        return new JdbcUserRepository(
+            jdbcOperations
+        );
     }
 
 }
