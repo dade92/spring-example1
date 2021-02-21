d813f37f0105ef036b7ad10b5006d289002cd40a
davide <davidebotti92@gmail.com>
2021-02-21 11:50:39 +0100

    Imeplements orders repository

--- src/main/kotlin/com/springexample/adapter/JdbcOrdersRepository.kt
+++ src/main/kotlin/com/springexample/adapter/JdbcOrdersRepository.kt
@@ -5,9 +5,13 @@ import arrow.core.Left
 import arrow.core.Right
 import com.springexample.domain.Order
 import com.springexample.domain.OrdersRepository
+import com.springexample.domain.User
 import org.slf4j.LoggerFactory
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
 import java.lang.RuntimeException
+import java.sql.ResultSet
+import java.util.*
 
 class JdbcOrdersRepository(
     private val jdbcTemplate: JdbcTemplate
@@ -17,7 +21,13 @@ class JdbcOrdersRepository(
 
     override fun save(order: Order, username: String): Either<RuntimeException, Unit> {
         return try {
-            jdbcTemplate.update("INSERT INTO ORDERS (TYPE, USER_ID) VALUES (?, ?)", order.type, 1)
+            val id = jdbcTemplate.queryForObject(
+                "SELECT ID FROM USERS WHERE USERNAME=?",
+                listOf(username).toTypedArray()
+            ) { resultSet: ResultSet?, _: Int ->
+                resultSet!!.getInt("ID")
+            }
+            jdbcTemplate.update("INSERT INTO ORDERS (TYPE, USER_ID) VALUES (?, ?)", order.type, id)
             Right(Unit)
         } catch (e: Exception) {
             logger.error("Cannot save order ", e)
--- src/test/kotlin/com/springexample/adapter/JdbcOrdersRepositoryTest.kt
+++ src/test/kotlin/com/springexample/adapter/JdbcOrdersRepositoryTest.kt
@@ -2,9 +2,7 @@ package com.springexample.adapter
 
 import arrow.core.Right
 import com.springexample.domain.Order
-import com.springexample.domain.User
 import org.hamcrest.CoreMatchers.`is`
-import org.junit.Assert
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
@@ -32,19 +30,25 @@ class JdbcOrdersRepositoryTest {
 
     @Test
     fun `adds order for a user`() {
+        insertUser(666, "davide", "XXX", "via vai")
+
         val result = jdbcOrdersRepository.save(Order("chair"), "davide")
 
-        assertThat(fetchRow("davide"), `is`(Order("chair")))
+        assertThat(fetchRow(), `is`(Order("chair")))
         assertThat(result, `is`(Right(Unit)))
     }
 
-    private fun fetchRow(username: String): Order {
+    private fun fetchRow(): Order {
         return jdbcTemplate.queryForObject(
-            "SELECT * FROM ORDERS WHERE USER_ID=1"
+            "SELECT * FROM ORDERS WHERE USER_ID=666"
         ) { resultSet: ResultSet, _: Int ->
             Order(
                 resultSet.getString("TYPE"),
             )
         }
+    }
+
+    private fun insertUser(id: Long, username: String, password: String, address: String) {
+        jdbcTemplate.update("INSERT INTO USERS VALUES (?, ?, ?, ?)", id, username, password, address)
     }
 }
