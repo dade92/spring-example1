74fe9ddee248c2577d5b507cd54dea5976564cf1
davide <davidebotti92@gmail.com>
2021-02-21 21:27:31 +0100

    Refactor

--- src/main/kotlin/com/springexample/webapp/OrdersConfiguration.kt
+++ src/main/kotlin/com/springexample/webapp/OrdersConfiguration.kt
@@ -1,6 +1,5 @@
 package com.springexample.webapp
 
-import com.springexample.adapter.JdbcOrdersRepository
 import com.springexample.domain.*
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
--- src/test/kotlin/com/springexample/adapter/JdbcOrdersRepositoryTest.kt
+++ src/test/kotlin/com/springexample/adapter/JdbcOrdersRepositoryTest.kt
@@ -2,6 +2,7 @@ package com.springexample.adapter
 
 import arrow.core.Left
 import arrow.core.Right
+import com.springexample.domain.JdbcOrdersRepository
 import com.springexample.domain.Order
 import com.springexample.domain.OrdersStoreError
 import org.hamcrest.CoreMatchers.`is`
