e972f62a748b0bee2281949e5a3d53b63f317509
davide <davidebotti92@gmail.com>
2021-11-29 14:24:23 +0100

    Refactor

--- src/main/kotlin/com/springexample/adapter/RestProductsRepository.kt
+++ src/main/kotlin/com/springexample/adapter/RestProductsRepository.kt
@@ -6,6 +6,7 @@ import arrow.core.Right
 import com.springexample.domain.Product
 import com.springexample.domain.ProductsRepository
 import com.springexample.domain.RetrieveProductsError
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.web.client.RestOperations
 
@@ -13,19 +14,24 @@ class RestProductsRepository(
     private val baseUrl: String,
     private val client: RestOperations
 ) : ProductsRepository {
-    override fun retrieveAll(): Either<RetrieveProductsError, List<Product>> {
-        try {
-            val result = client.getForEntity(baseUrl + "/products", ProductsResponse::class.java)
 
-            return if(result.statusCode==HttpStatus.OK) {
-                Right(result.body.products.map { Product(it.name) })
+    private val logger = LoggerFactory.getLogger(RestProductsRepository::class.java)
+
+    override fun retrieveAll(): Either<RetrieveProductsError, List<Product>> =
+        try {
+            val result = client.getForEntity("$baseUrl/products", ProductsResponse::class.java)
+
+            if (result.statusCode == HttpStatus.OK) {
+                Right(result.body.products.map { adaptProduct(it) })
             } else {
                 Left(RetrieveProductsError.RestError)
             }
         } catch (e: Exception) {
-            return Left(RetrieveProductsError.RestError)
+            logger.error("Failed to retrieve products", e)
+            Left(RetrieveProductsError.RestError)
         }
-    }
+
+    private fun adaptProduct(restProduct: RestProduct) = Product(restProduct.name)
 }
 
 data class ProductsResponse(
