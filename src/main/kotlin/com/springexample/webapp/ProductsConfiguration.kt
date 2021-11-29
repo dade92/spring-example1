5f500362c768740f6fc2b0d3ee62b425c8da2f49
davide <davidebotti92@gmail.com>
2021-11-29 14:55:17 +0100

    Adds configuration file for products service

--- src/main/kotlin/com/springexample/webapp/ProductsConfiguration.kt
+++ src/main/kotlin/com/springexample/webapp/ProductsConfiguration.kt
@@ -4,13 +4,16 @@ import com.springexample.adapter.RestProductsRepository
 import com.springexample.domain.DefaultRetrieveProductsUseCase
 import com.springexample.domain.ProductsRepository
 import com.springexample.domain.RetrieveProductsUseCase
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
 
 @Configuration
+@EnableConfigurationProperties(
+    ProductsProperties::class
+)
 open class ProductsConfiguration {
-
     @Bean
     open fun retrieveProductsUseCase(
         productsRepository: ProductsRepository
@@ -18,6 +21,8 @@ open class ProductsConfiguration {
         DefaultRetrieveProductsUseCase(productsRepository)
 
     @Bean
-    open fun productsRepository(): ProductsRepository =
-        RestProductsRepository("http://localhost:8081", RestTemplate())
+    open fun productsRepository(
+        productsProperties: ProductsProperties
+    ): ProductsRepository =
+        RestProductsRepository(productsProperties.url, RestTemplate())
 }
