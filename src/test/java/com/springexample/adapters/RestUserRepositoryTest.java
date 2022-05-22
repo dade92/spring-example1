d24e6729d4ffbdb870aa46d573d53c42df522f0f
davide <davidebotti92@gmail.com>
2022-05-22 18:01:11 +0200

    Updates Wiremock for JUnit 5

--- src/test/java/com/springexample/adapters/RestUserRepositoryTest.java
+++ src/test/java/com/springexample/adapters/RestUserRepositoryTest.java
@@ -1,21 +1,29 @@
 package com.springexample.adapters;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import com.springexample.domain.User;
 import com.springexample.utils.Fixtures;
-import org.junit.Before;
 import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class RestUserRepositoryTest {
+
+    @RegisterExtension
+    static WireMockExtension wm1 = WireMockExtension.newInstance()
+        .options(wireMockConfig().dynamicPort().dynamicHttpsPort())
+        .build();
 
     public static final String ADD_USER_REST_REQUEST = Fixtures.Companion.readJson("/addUserRestRequest.json");
     public static final String FETCH_USER_RESPONSE = Fixtures.Companion.readJson("/fetchUserResponse.json");
@@ -25,17 +33,17 @@ public class RestUserRepositoryTest {
 
     private RestUserRepository restUserRepository;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         restUserRepository = new RestUserRepository(
-            "http://localhost:8089",
+            "http://localhost:" + wm1.getRuntimeInfo().getHttpPort(),
             new RestTemplate()
         );
     }
 
     @Test
     public void fetch() {
-        stubFor(get(urlEqualTo("/user/666"))
+        wm1.stubFor(get(urlEqualTo("/user/666"))
             .willReturn(okJson(FETCH_USER_RESPONSE)));
 
         Optional<User> user = restUserRepository.fetch(666L);
@@ -45,7 +53,7 @@ public class RestUserRepositoryTest {
 
     @Test
     public void fetchFailsWith4xx() {
-        stubFor(get(urlEqualTo("/user/666"))
+        wm1.stubFor(get(urlEqualTo("/user/666"))
             .willReturn(status(HttpStatus.BAD_REQUEST.value())));
 
         assertThat(restUserRepository.fetch(666L), is(Optional.empty()));
@@ -53,7 +61,7 @@ public class RestUserRepositoryTest {
 
     @Test
     public void fetchFailsWith5xx() {
-        stubFor(get(urlEqualTo("/user/666"))
+        wm1.stubFor(get(urlEqualTo("/user/666"))
             .willReturn(status(HttpStatus.INTERNAL_SERVER_ERROR.value())));
 
         assertThat(restUserRepository.fetch(666L), is(Optional.empty()));
@@ -61,7 +69,7 @@ public class RestUserRepositoryTest {
 
     @Test
     public void addUserSuccessfully() {
-        stubFor(post(urlEqualTo("/addUser"))
+        wm1.stubFor(post(urlEqualTo("/addUser"))
             .withRequestBody(equalToJson(ADD_USER_REST_REQUEST))
             .willReturn(ok()));
 
@@ -72,7 +80,7 @@ public class RestUserRepositoryTest {
 
     @Test
     public void addUserFailsWith4xx() {
-        stubFor(post(urlEqualTo("/addUser"))
+        wm1.stubFor(post(urlEqualTo("/addUser"))
             .withRequestBody(equalToJson(ADD_USER_REST_REQUEST))
             .willReturn(status(400)));
 
@@ -83,7 +91,7 @@ public class RestUserRepositoryTest {
 
     @Test
     public void addUserFailsWith5xx() {
-        stubFor(post(urlEqualTo("/addUser"))
+        wm1.stubFor(post(urlEqualTo("/addUser"))
             .withRequestBody(equalToJson(ADD_USER_REST_REQUEST))
             .willReturn(status(500)));
 
--- src/test/kotlin/com/springexample/adapter/RestProductsRepositoryTest.kt
+++ src/test/kotlin/com/springexample/adapter/RestProductsRepositoryTest.kt
@@ -3,56 +3,64 @@ package com.springexample.adapter
 import arrow.core.Left
 import arrow.core.Right
 import com.github.tomakehurst.wiremock.client.WireMock
-import com.github.tomakehurst.wiremock.junit.WireMockRule
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension
 import com.springexample.domain.Product
 import com.springexample.domain.RetrieveProductsError
 import com.springexample.utils.Fixtures.Companion.readJson
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
 import org.springframework.web.client.RestTemplate
+
 
 class RestProductsRepositoryTest {
 
-    @Rule
+    @RegisterExtension
     @JvmField
-    var wireMockRule = WireMockRule(8089)
+    var wm1: WireMockExtension = WireMockExtension.newInstance()
+        .options(wireMockConfig().dynamicPort().dynamicHttpsPort())
+        .build()
 
     private val FETCH_PRODUCTS_RESPONSE = readJson("/fetchProducts.json")
 
     private lateinit var restProductsRepository: RestProductsRepository
 
-    @Before
+    @BeforeEach
     fun setUp() {
         restProductsRepository = RestProductsRepository(
-            "http://localhost:8089",
+            "http://localhost:" + wm1.runtimeInfo.httpPort,
             RestTemplate()
         )
     }
 
     @Test
     fun `retrieve Products`() {
-        WireMock.stubFor(
+        wm1.stubFor(
             WireMock.get(WireMock.urlEqualTo("/products"))
                 .willReturn(WireMock.okJson(FETCH_PRODUCTS_RESPONSE))
         )
 
         val result = restProductsRepository.retrieveAll()
 
-        Assert.assertThat(result, `is`(Right(
-            listOf(
-                Product("Chair", true),
-                Product("Sofa", true),
-                Product("Television", true)
+        Assert.assertThat(
+            result, `is`(
+                Right(
+                    listOf(
+                        Product("Chair", true),
+                        Product("Sofa", true),
+                        Product("Television", true)
+                    )
+                )
             )
-        )))
+        )
     }
 
     @Test
     fun `retrieve products fails`() {
-        WireMock.stubFor(
+        wm1.stubFor(
             WireMock.get(WireMock.urlEqualTo("/products"))
                 .willReturn(WireMock.serverError())
         )
