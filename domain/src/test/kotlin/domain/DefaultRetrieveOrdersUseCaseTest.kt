5ccd6fc31cf59661248f3d0f92ba207ed3ff1f41
dbotti <davide.botti@lastminute.com>
2022-11-26 10:34:48 +0100

    Populate domain module

---
+++ domain/src/test/kotlin/domain/DefaultRetrieveOrdersUseCaseTest.kt
@@ -0,0 +1,43 @@
+package domain
+
+import arrow.core.Right
+import org.hamcrest.CoreMatchers.`is`
+import org.jmock.AbstractExpectations.returnValue
+import org.jmock.Expectations
+import org.jmock.auto.Mock
+import org.jmock.junit5.JUnit5Mockery
+import org.junit.Assert.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
+
+class DefaultRetrieveOrdersUseCaseTest {
+
+    @RegisterExtension
+    @JvmField
+    val context = JUnit5Mockery()
+
+    @Mock
+    private lateinit var ordersRepository: OrdersRepository
+
+    private lateinit var defaultRetrieveOrdersUseCase: DefaultRetrieveOrdersUseCase
+
+    @BeforeEach
+    fun setUp() {
+        defaultRetrieveOrdersUseCase = DefaultRetrieveOrdersUseCase(ordersRepository)
+    }
+
+    @Test
+    fun `retrieve orders`() {
+        val expectedResult = Right(listOf(Order("chair")))
+
+        context.checking(Expectations().apply {
+            oneOf(ordersRepository).retrieve("Davide")
+            will(returnValue(expectedResult))
+        })
+
+        val result = defaultRetrieveOrdersUseCase.retrieve("Davide")
+
+        assertThat(result, `is`(expectedResult))
+    }
+}
---
+++ domain/src/test/kotlin/domain/DefaultSaveOrdersUseCaseTest.kt
@@ -0,0 +1,43 @@
+package domain
+
+import arrow.core.Right
+import org.hamcrest.CoreMatchers.`is`
+import org.jmock.AbstractExpectations.returnValue
+import org.jmock.Expectations
+import org.jmock.auto.Mock
+import org.jmock.junit5.JUnit5Mockery
+import org.junit.Assert.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
+
+class DefaultSaveOrdersUseCaseTest {
+
+    @RegisterExtension
+    @JvmField
+    val context = JUnit5Mockery()
+
+    @Mock
+    private lateinit var ordersRepository: OrdersRepository
+
+    private lateinit var defaultSaveOrdersUseCase: SaveOrdersUseCase
+
+    @BeforeEach
+    fun setUp() {
+        defaultSaveOrdersUseCase = DefaultSaveOrdersUseCase(ordersRepository)
+    }
+
+    @Test
+    fun `save order for a user`() {
+        val username = "davide"
+        val order = Order("chair")
+        val expectedResult = Right(Unit)
+
+        context.checking(Expectations().apply {
+            oneOf(ordersRepository).save(order, username)
+            will(returnValue(expectedResult))
+        })
+
+        assertThat(defaultSaveOrdersUseCase.execute(username, order), `is`(expectedResult))
+    }
+}
