0f27878206e8e185ed3662ce645daa8a32e0926e
dbotti <davide.botti@lastminute.com>
2022-12-01 14:38:12 +0100

    Reformat

--- webapp/src/main/kotlin/webapp/ProductsConfiguration.kt
+++ webapp/src/main/kotlin/webapp/ProductsConfiguration.kt
@@ -9,9 +9,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableConfigurationProperties(
-    ProductsProperties::class
-)
+@EnableConfigurationProperties(ProductsProperties::class)
 class ProductsConfiguration {
 
     @Bean
