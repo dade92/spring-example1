4b89ada9670bd2354e51b13cde8e7240a77f1d82
davide <davidebotti92@gmail.com>
2022-12-24 12:42:59 +0100

    First producer working example

--- webapp/src/main/java/webapp/AppConfiguration.java
+++ webapp/src/main/java/webapp/AppConfiguration.java
@@ -1,8 +1,10 @@
 package webapp;
 
+import adapters.KafkaProducer;
 import domain.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @Configuration
 public class AppConfiguration {
@@ -15,5 +17,10 @@ public class AppConfiguration {
     @Bean
     public SaveUserUseCase saveUserUseCase(UserRepository userRepository) {
         return new DefaultSaveUserUseCase(userRepository);
+    }
+
+    @Bean
+    public KafkaProducer kafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        return new KafkaProducer(kafkaTemplate);
     }
 }
