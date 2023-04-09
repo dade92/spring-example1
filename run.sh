25e33b1b6a2f78986fd595a41166bad46f9ab893
davide <davidebotti92@gmail.com>
2023-04-09 11:44:57 +0200

    Adds run and stop deploy scripts

---
+++ run.sh
@@ -0,0 +1,16 @@
+echo "Starting database..."
+docker compose up -d mysqldb
+sleep 2
+
+echo "Starting external catalog..."
+docker compose up -d external-catalog
+
+echo "Starting backend app..."
+docker compose up -d app
+sleep 2
+
+docker ps
+
+echo "App seems up and running. Enjoy!"
+
+
