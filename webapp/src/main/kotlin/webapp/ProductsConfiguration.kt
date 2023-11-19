d5d1e06fae700170ad70ae8d0e64c330b98e1f05
Davide Botti <davidebotti92@gmail.com>
2023-11-19 16:47:43 +0100

    Removes wrong dependency from adapters in the webapp module

--- webapp/src/main/kotlin/webapp/ProductsConfiguration.kt
+++ webapp/src/main/kotlin/webapp/ProductsConfiguration.kt
@@ -1,20 +1,15 @@
 package webapp
 
-import adapters.configuration.ProductsProperties
 import domain.DefaultRetrieveProductsUseCase
 import domain.ProductsRepository
 import domain.RetrieveProductsUseCase
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableConfigurationProperties(ProductsProperties::class)
 class ProductsConfiguration {
 
     @Bean
-    fun retrieveProductsUseCase(
-        productsRepository: ProductsRepository
-    ): RetrieveProductsUseCase =
+    fun retrieveProductsUseCase(productsRepository: ProductsRepository): RetrieveProductsUseCase =
         DefaultRetrieveProductsUseCase(productsRepository)
 }
