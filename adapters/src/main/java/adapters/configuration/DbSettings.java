fffb31cbd817b5ec1aeb147076ef8b79c562a439
Davide Botti <davidebotti92@gmail.com>
2024-12-29 18:20:56 +0100

    Remove useless param in the properties

--- adapters/src/main/java/adapters/configuration/DbSettings.java
+++ adapters/src/main/java/adapters/configuration/DbSettings.java
@@ -4,16 +4,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("spring.datasource.spring-example")
 public class DbSettings {
-    public String url;
     public String host;
     public String port;
     public String database;
     public String username;
     public String password;
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
 
     public void setUsername(String username) {
         this.username = username;
